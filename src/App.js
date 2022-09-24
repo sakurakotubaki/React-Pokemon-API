0fbfa01f472d9fb6e84c9a66241f8268882be83a
[Flutter_SakuSaku13:44] <[gitonly543@gmail.com]>
2022-09-24 11:12:03 +0900

    ポケモンの詳細データを取得できた

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
 import { useEffect, useState } from 'react';
 import './App.css';
-import { getAllPokemon } from "./utils/pokemon";
+import { getAllPokemon, getPokemon } from "./utils/pokemon";
 
 function App() {
   // ポケモンAPIのURL
   const initialURL = "https://pokeapi.co/api/v2/pokemon";
   // 最初からデータを取得するのでtrueにする
   const [loading, setLoading] = useState(true);
+  // ポケモンデータを格納する変数
+  const [pokemonData, setPokemonData] = useState([]);
 
   // 一度だけ呼び出すので、第二引数にからの配列を渡す[]
   useEffect(() => {
     const fetchPokemonData = async () => {
       //全てのポケモンデータを取得
       let res = await getAllPokemon(initialURL);
-      console.log(res);
+      //各ポケモンの詳細なデータを取得
+      loadPokemon(res.results);
+      // console.log(res.results);
       setLoading(false);
     };
     // 関数を呼ぶ
     fetchPokemonData();
   },[]);
+
+  const loadPokemon = async (data)  => {
+    // 全てのデータを取得する
+    let _pokemonData = await Promise.all(
+      data.map((pokemon) => {
+        // console.log(pokemon);
+        let pokemonRecode = getPokemon(pokemon.url);
+        return pokemonRecode;
+      })
+    );
+    setPokemonData(_pokemonData);
+  };
+  // useStateのデータをログに表示
+  console.log(pokemonData);
 
   return (
     <div className="App">
--- src/utils/pokemon.js
+++ src/utils/pokemon.js
@@ -6,3 +6,14 @@ export const getAllPokemon = (url) => {
     .then((data) => resolve(data));
   });
 };
+// ポケモンのデータをとってくる関数
+export const getPokemon = (url) => {
+  return new Promise((resolve, reject) => {
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        resolve(data);
+      });
+  });
+};
