befcb332e73bda006e0088b6d3e41cee09863b05
[Flutter_SakuSaku13:44] <[gitonly543@gmail.com]>
2022-09-24 10:26:07 +0900

    ポケモンAPIのデータを取得に成功

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
+import { useEffect } from 'react';
 import './App.css';
+import { getAllPokemon } from "./utils/pokemon";
 
 function App() {
+  const initialURL = "https://pokeapi.co/api/v2/pokemon";
+
+  useEffect(() => {
+    const fetchPokemonData = async () => {
+      //全てのポケモンデータを取得
+      let res = await getAllPokemon(initialURL);
+      console.log(res);
+    };
+    // 関数を呼ぶ
+    fetchPokemonData();
+  },[]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
     </div>
   );
 }
